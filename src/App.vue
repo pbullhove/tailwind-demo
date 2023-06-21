7855f715a59d9d309b678d3ca165232208c77fac
pbullhove <peterbullhove@gmail.com>
2023-06-21 15:05:00 +0200

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <NavBar />
   <div class="flex flex-col items-center gap-4 pt-4">
-    <div class="">
+    <div class="flex max-w-screen-xl flex-col gap-4 gap-y-6 sm:m-4 sm:grid sm:grid-cols-2 lg:grid-cols-3">
       <Video v-for="video in videos" :key="video.title" :video="video" />
     </div>
     <!-- <div
--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="">
-    <img class="" :src="props.video.thumbnail" alt="Thumbnail" />
-    <img class="" :src="props.video.icon" alt="Channel icon" />
-    <p class="">{{ props.video.title }}</p>
-    <p class="">{{ props.video.channel }}</p>
-    <p class="">{{ props.video.views }} views</p>
-    <p class="">{{ props.video.publishedAt }}</p>
+  <div class="hover:bg-gray-800 md:rounded-2xl cursor-pointer">
+    <img class="md:rounded-2xl " :src="props.video.thumbnail" alt="Thumbnail" />
+    <div class="flex items-start p-2 gap-4">
+      <img class="rounded-full w-12 h-12" :src="props.video.icon" alt="Channel icon" />
+      <div class="flex flex-col">
+        <p class="font-bold">{{ props.video.title }}</p>
+        <p class="text-gray-400">{{ props.video.channel }}</p>
+        <div class="flex gap-4">
+          <p class="text-gray-400">{{ props.video.views }} views</p>
+          <p class="text-gray-400">{{ props.video.publishedAt }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
